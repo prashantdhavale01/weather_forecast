2714a22bfd5a912668d3bd4bdbc8eaba36ff41a8
prashantboot <prashant.d@bootinfotech.com>
2019-02-14 11:43:39 +0530

    validate mandatory fields

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,5 +1,38 @@
 $(document).ready(function($) {
  $("#btnRegister").on('click', (e) => {
-  alert();
+  if (isValidate()) {
+    if (validateEmail($('#email_address').val()) && telephoneCheck($('#phone_number').val())) {
+      $('#email_address, #phone_number').removeClass("emptyField");
+    } else {
+      $('#email_address, #phone_number').addClass("emptyField");
+      return;
+    } 
+    alert("Registration Successfully")
+  }else {
+    alert("All fields are mandatory");
+  }
 }); 
+ function isValidate() {
+  var isValid = true;     
+  const allFields = ["#full_name", "#email_address", "#phone_number"];
+  for (let field of allFields) {
+    if ($(field).val().trim().length == 0) {
+      $(field).addClass('emptyField');
+      $(field).on('input', () => {
+        $(field).removeClass('emptyField');
+      });
+      isValid = false;
+    }
+  }
+  return isValid;
+}
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+function telephoneCheck(phone) {
+  var isphone = /^[+]?(1\-|1\s|1|\d{3}\-|\d{3}\s|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/g.test(phone);
+  console.log(isphone);
+  return isphone;
+}
 });
